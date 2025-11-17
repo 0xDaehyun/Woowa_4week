7c863cd9c7c966d085f550915f821643d3b99d21
이대현 <leedaehyun11@naver.com>
2025-11-17 17:27:17 +0900

    feat: 서버와 클라이언트 관계 정립하기

--- Client.py
+++ Client.py
@@ -1,4 +1,14 @@
 from socket import *;
 
-clientSock = socket(AF_INET , SOCK_STREAM);
-clientSock.connect(('127.0.0.1' , 8080));
+clientSock = socket(AF_INET , SOCK_STREAM)  # 서버와 연결할 소켓 객체를 생성
+clientSock.connect(('127.0.0.1' , 8080)) #ip주소와 port 번호로 서버와 연결을 요청해 수락을 기다린다 .
+
+# 127.0.0.1은 자기 자신을 의미한다.
+# 위의 어드레스 패밀리는 자기 자신에게 8080번 포트로 연결하란 소리가 되겠네요.
+
+print('서버와 연결 완료!')
+clientSock.send('안녕하세요 저는 클라이언트 입니다.'.encode('utf-8'))
+print('전송 완료.')
+
+recieveData = clientSock.recv(1024)
+print(recieveData.decode('utf-8'))
--- Server.py
+++ Server.py
@@ -1,19 +1,24 @@
 from socket import *;
 
 serverSock = socket(AF_INET , SOCK_STREAM)
+# 소켓 객체를 생성할때는 어드레스 패밀리 , 소켓 타입의 인자를 두개 받아야 한다
 
 serverSock.bind(('', 8080))
-# 클라이언트를 만들떄는 불필요하며.
-# 서버를 운용할떄는 반드시 필요하다.
-#'' 으로 주소에 해당되는 부분을 비워놓는 이유는 모든 인터페이스를 연결한다는 뜻이다.
+# 바인드는 소켓과 AF를 연결하는 과정이라 했다. 이 인자는 어드레스 패밀리가 된다
+# 클라이언트를 만들떄는 불필요하며. 서버를 운용할떄는 반드시 필요하다.
+# ' ' 으로 주소에 해당되는 부분을 비워놓는 이유는 모든 인터페이스를 연결한다는 뜻이다.
 #브로드캐스트를 하고 싶다면 '' 으로 !
 serverSock.listen(1)
-#
-connectionSoc, addr = serverSock.accept()
+#상대방의 접속을 기다리겠다는 뜻이더.
+
+connectionSock, addr = serverSock.accept()
 # 누군가가 접속하여 연결 하였을 때에 비로소 결과값이 return 되는 함수이다.
+# return 값으로 새로운 소켓과, 상대방의 AF를 전달해주게 됩니다.
+
 
 print(str(addr) , '에서 접속이 확인되었습니다')
-data = connectionSoc.recv(1024)
+data = connectionSock.recv(1024)
+print('받은 데이터: ' , data.decode('utf-8'))
 
-
- 
+connectionSock.send('서버에서 보낸다! '.encode('utf-8'))
+print('서버에서 메시지 전송 완료')
